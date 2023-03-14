8bd080b620606cf26ff06fc00cd9997f97647f6e
NicoSeddiki <nseddiki@sipfront.com>
2023-03-14 12:21:20 +0100

    Added logger

--- main.go
+++ main.go
@@ -9,13 +9,32 @@ import (
 	"fmt"
 	"os"
 
-	opensearch "github.com/opensearch-project/opensearch-go/v2"
 	// opensearchapi "github.com/opensearch-project/opensearch-go/v2/opensearchapi"
+	opensearch "github.com/opensearch-project/opensearch-go/v2"
+	log "github.com/sirupsen/logrus"
 )
 
 const IndexName = "sipfront-go-test"
 
+// Initialization of the logrus logger -> set-up o
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+}
+
 func main() {
+	log.WithFields(log.Fields{
+		"dummy-field-1": "fizz",
+		"dummy-field-2": "buzz",
+		"dummy-field-3": "fizzbuzz",
+	}).Info("this-is-a-test")
 
 	// Initialize the client with SSL/TLS enabled.
 	var clientConfiguration opensearch.Config = opensearch.Config{
