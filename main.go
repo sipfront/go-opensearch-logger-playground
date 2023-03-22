15982184c5594ec598f7005aaf92949ca85ffc7e
NicoSeddiki <nseddiki@sipfront.com>
2023-03-22 10:31:37 +0100

    Figure out why malformed json

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ import (
 // For test purposes
 const IndexName = "sipfront-gotest-v2-2023.03.22"
 
+var replacer = strings.NewReplacer("\n", "", " ", "")
+
 // ------------------------------------------------------------------------------------------------
 // Custom type which will later implement the Write method for logging directly to
 // Opensearch, without the help of using logstash.
@@ -27,16 +29,20 @@ type OpenSearchWriter struct {
 // Writer interface to log directly to opensearch. Based on [SO-Post]
 // [SO-Post] https://bit.ly/3Tj0fqe
 func (ow *OpenSearchWriter) Write(p []byte) (n int, err error) {
-	var document *strings.Reader = strings.NewReader(string(p))
-	req := opensearchapi.IndexRequest{
+	// https://stackoverflow.com/questions/45792309/bulk-api-malformed-action-metadata-line-3-expected-start-object-but-found
+	mapping := `{"mappings":{"properties":{"@timestamp":{"type":"date","format":"yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"},"function_name":{"type":"text"}}}}`
+
+	fmt.Printf("%s\n%s\n", mapping, replacer.Replace(string(p)))
+	var document *strings.Reader = strings.NewReader(fmt.Sprintf("%s\n%s\n", mapping, replacer.Replace(string(p))))
+	req := opensearchapi.BulkRequest{
 		Index: IndexName,
 		Body:  document,
 	}
-
 	insertResponse, err := req.Do(context.Background(), ow.Client)
 	if err != nil {
 		return 0, err
 	}
+	defer insertResponse.Body.Close()
 	fmt.Println(insertResponse)
 
 	return len(p), nil
@@ -64,36 +70,11 @@ func main() {
 	// Print OpenSearch version information on console.
 	fmt.Println(client.Info())
 
-	// IGNORE: Noy needed for creating an index document!
-	// Define index mapping.
-	mapping := strings.NewReader(`{
-		"mappings":{
-			"properties":{
-				"@timestamp":{
-					"type":"date",
-					"format":"yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"},
-				"function_name":{"type":"text"}
-			}
-		}
-	}`)
-	// Create an index with non-default settings.
-	res := opensearchapi.IndicesCreateRequest{
-		Index: IndexName,
-		Body:  mapping,
-	}
-	insertResponse, err := res.Do(context.Background(), client)
-	if err != nil {
-		fmt.Println("cannot create", err)
-		os.Exit(1)
-	}
-	fmt.Println(insertResponse)
-
 	// Set Up Logger ----------------------------------------------------------------------------
 	var l *logrus.Logger = &logrus.Logger{
 		Out:   &OpenSearchWriter{Client: client},
 		Level: logrus.InfoLevel,
 		Formatter: &OpensearchFormatter{
-			// DataKey:           "labels",
 			DisableHTMLEscape: true,
 			PrettyPrint:       true,
 		},
