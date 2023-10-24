67b90c39c8013379c0729d0b6d5a107d2126a3c8
NicoSeddiki <nseddiki@sipfront.com>
2023-10-24 10:55:51 +0200

    Copied logSink from https://github.com/sipfront/terraform/blob/mseidl/persistor_log_rework/function-persistor/lambda/src/main.go#L1438

--- main.go
+++ main.go
@@ -17,6 +17,108 @@ import (
 )
 
 //-------------------------------------------------------------------------------------------------
+type SF_LogLevel int64
+type SF_LogMessage struct {
+	AwsRequestId 	string    		`json:"aws_request_id,omitempty"`
+	Message  		string			`json:"message"`
+	LogLevel 		SF_LogLevel		`json:"level"`
+	Timestamp    	time.Time		`json:"@timestamp"`
+}
+
+const (
+	SF_LogLevel_Info SF_LogLevel = iota
+	SF_LogLevel_Error
+)
+
+var logc chan SF_LogMessage
+
+
+//-------------------------------------------------------------------------------------------------
+func log_info(aws_request_id, message string) {
+	logc <- SF_LogMessage{
+		AwsRequestId: 	aws_request_id,
+		LogLevel: 		SF_LogLevel_Info,
+		Message: 		message,
+		Timestamp: 		time.Now.UTC(),
+	}
+}
+
+//-------------------------------------------------------------------------------------------------
+func log_error(message string) {
+	logc <- SF_LogMessage{
+		AwsRequestId: 	aws_request_id,
+		LogLevel: 		SF_LogLevel_Error,
+		Message: 		message,
+		Timestamp: 		time.Now.UTC(),
+	}
+}
+
+//-------------------------------------------------------------------------------------------------
+func logSink(
+	ctx context.Context,
+	function_name string,
+	aws_request_id string,
+	in <-chan SF_LogMessage) (<-chan error, error) {
+
+	errc := make(chan error, 1)
+	go func() {
+		defer close(errc)
+
+		// create logger!
+		var go_logger_client *logrus.Logger = nil
+		var go_logger *logrus.Entry = nil
+
+		if go_logger_client == nil {
+			go_logger_client = logrus.New()
+			fields := logrus.Fields{
+				"function_name":  function_name,
+				"aws_request_id": aws_request_id,
+			}
+			go_logger = go_logger_client.WithFields(fields)
+
+			// Set Up OpenSearch Client and initialize with SSL/TLS enabled.
+			var clientConfiguration opensearch.Config = opensearch.Config{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+				},
+				Addresses: []string{
+					es_endpoint},
+			}
+			client, err := opensearch.NewClient(clientConfiguration)
+			if err != nil {
+				println("init logging failed")
+				panic("init logging failed")
+			} else {
+				// Set Up logger with non-default params
+				// logger_client.SetOutput(&OpenSearchWriter{Client: client})
+				logger_client.SetFormatter(&OpensearchFormatter{})
+				logger_client.SetLevel(logrus.InfoLevel)
+			}
+		}
+
+		for {
+			select {
+			case s := <-in:
+				switch level := s.LogLevel; level {
+				case SF_LogLevel_Info:
+					//log info
+					//log_info("logSink: " + s.Message)
+					go_logger.Info("logSink: " + s.Message)
+				case SF_LogLevel_Error:
+					//log error
+					//logger.Info("logSink: " + s.Message)
+					go_logger.Error("logSink: " + s.Message)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return errc, nil
+}
+
+
+//-------------------------------------------------------------------------------------------------
 // LogMessage describes a simple log message, which is then encoded into a json
 type LogMessage struct {
 	Function     	string    	`json:"function_name"`
@@ -30,7 +132,7 @@ type LogMessage struct {
 }
 
 //-------------------------------------------------------------------------------------------------
-// Custom type that will later implement the Write method/interface for logging 
+// Custom type that will later implement the Write method/interface for logging
 // directly to Opensearch, without the help of using logstash.
 type OpenSearchWriter struct {
 	Client *opensearch.Client
--- opensearch-logging-go-logrus.go
+++ opensearch-logging-go-logrus.go
@@ -78,7 +78,7 @@ func (f *OpensearchFormatter) Format(e *logrus.Entry) ([]byte, error) {
 	jf := logrus.JSONFormatter{
 		TimestampFormat: "2006-01-02T15:04:05.999999999Z07:00",
 		FieldMap: logrus.FieldMap{
-			logrus.FieldKeyMsg: "message", 
+			logrus.FieldKeyMsg: "message",
 			logrus.FieldKeyTime: "@timestamp",
 		},
 		DisableHTMLEscape: f.DisableHTMLEscape,
